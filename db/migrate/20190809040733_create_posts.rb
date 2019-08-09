6835e39cdecf30042806f57d7ea67c5176a0192a
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-08 21:40:07 -0700

    migrated and updated create posts for video

--- db/migrate/20190809040733_create_posts.rb
+++ db/migrate/20190809040733_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.text :content
+      t.string :video
       t.belongs_to :author, index: true
       t.belongs_to :state, index: true
       t.belongs_to :city, index: true
