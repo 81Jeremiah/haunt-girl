d52d258535e8e7bf148255bae626e1ba481296da
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-08 21:27:02 -0700

    migrations for models

---
+++ db/migrate/20190809040733_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.belongs_to :author, index: true
+      t.belongs_to :state, index: true
+      t.belongs_to :city, index: true
+      t.datetime :published_at
+      t.timestamps
+    end
+  end
+end
