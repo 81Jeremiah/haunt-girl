0e4029e81beb7e21d05e264fcd8393397a206702
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-13 21:43:21 -0700

    set up redux and dev tools to index

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/index.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    //Redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
+)
+//provider gives access to store to all children of App
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
