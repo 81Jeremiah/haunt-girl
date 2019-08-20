a338070114bf88d2600213a88b2cd27f7b2cc86f
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-19 20:49:27 -0700

    added main spalsh components

--- client/src/containers/Splash.js
+++ client/src/containers/Splash.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import Featured from '../components/Featured';
+import ShortBio from '../components/ShortBio';
+import Footer from '../components/Footer';
+import BlogSection from '../components/BlogSection';
+import FeaturedSection from '../components/FeaturedSection';
+import NewsletterSection from '../components/NewsletterSection'
+import FeaturedCarousel from '../components/FeaturedCarousel'
 
 const Splash = () =>{
 
   return(
     <div>
+      <Navbar />
+      <FeaturedCarousel />
+      <ShortBio />
+      <Featured />
+      <BlogSection />
+      <FeaturedSection />
+      <NewsletterSection />
+      <Footer />
+
     </div>
   );
 }
+
+
+export default Splash
