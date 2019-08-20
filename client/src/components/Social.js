a2c9fe1a4caf07548d2a0b1f75db2449b1ec380e
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-19 20:49:08 -0700

    moved social links to one component to stay DRY

---
+++ client/src/components/Social.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Social = () => {
+  return(
+    <div>
+      <a href="https://www.youtube.com/channel/UCYxmZvq8qVQyq28g5W1EFlQ" target="blank"><i class="fa fa-youtube"></i></a>
+      <a href="https://www.instagram.com/thehauntgirl" target="blank"><i class="fa fa-instagram"></i></a>
+      <a href="https://twitter.com/christinebarger" targer="blank"><i class="fa fa-twitter"></i></a>
+    </div>
+  )
+}
+
+export default Social
