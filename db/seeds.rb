98d0babb7cfd0047f28c83cef2dbd6423bb99885
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-20 22:11:39 -0700

    added post serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create([{title:'title1', content: "stuff stuff stuff"},{title:'title2', content: "stuff stuff stuff"}  ])
