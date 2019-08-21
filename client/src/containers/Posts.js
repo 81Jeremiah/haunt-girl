cb8bd0e76a3e88a68c5c8b6284101b1662813b12
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-20 22:13:22 -0700

    added post container to fetch posts

---
+++ client/src/containers/Posts.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {fetchPosts} from '../actions/postActions';
+import {connect} from 'react-redux';
+import PostsList from '../components/PostsList';
+
+class Posts extends Component {
+  componentWillMount(){
+
+    this.props.fetchPosts()
+
+  }
+  render(){
+    return(
+      <div>
+        <PostsList posts={this.props.posts} />
+      </div>
+
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps, {fetchPosts})(Posts);
