d854642eb3f185d5bef8d85b4648b23d656e8689
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-23 22:15:51 -0700

    added buttons and state for new post form

--- client/src/components/admin/PostForm.js
+++ client/src/components/admin/PostForm.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
-import Toolbar from './Toolbar'
+import Toolbar from './Toolbar';
+import Button from 'react-bootstrap/Button';
+
 class PostForm extends Component {
   state = {
+    title:"",
+    content:"",
+    category:""
 
+  }
+
+  handleChange = event => {
+    const {name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
   }
 
   handleSubmit = () =>{
@@ -17,21 +29,23 @@ class PostForm extends Component {
       <Form onSubmit={this.handleSubmit}>
         <Form.Group controlId="exampleForm.ControlInput1">
           <Form.Label>Title</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Control size="lg" type="text" value={this.state.title} onChange={this.handleChange} name="title"/>
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlSelect1">
           <Form.Label>Category</Form.Label>
-          <Form.Control as="select">
+          <Form.Control as="select" value={this.state.category} onChange={this.handleChange} name="category">
             <option>Haunts</option>
             <option>Escape Rooms</option>
             <option>Oddities</option>
           </Form.Control>
         </Form.Group>
         <Form.Group controlId="">
-        
+
           <Form.Label>Blog Post</Form.Label>
-          <Form.Control as="textarea" rows="25" />
+          <Form.Control as="textarea" rows="15" value={this.state.content} onChange={this.handleChange} name="content"/>
         </Form.Group>
+          <Button variant="primary" type="submit" value="New Entry">New Entry</Button>
+
         </Form>
 
       </div>
