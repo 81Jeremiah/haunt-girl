cab276b844ee7c43aa8c300e163090e5e02f255e
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-24 19:10:04 -0700

    added create and custom actions to post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::PostsController < ApplicationController
+  before_action :set_post, only: [:update, :show]
 
   def index
     @posts = Post.all
@@ -6,9 +7,35 @@ class Api::PostsController < ApplicationController
   end
 
   def show
+    render json: @post
+  end
+
+  def create
+    @post= Post.create!(post_params)
 
   end
 
+  def escape_rooms
+    # @escape_rooms = Post.find_by(category:)
+  end
+  
+  def new
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def post_params
+    params.require(:post).premit(:title, :category, :content)
+  end
 
 
 end
