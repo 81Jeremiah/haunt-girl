c33bd195a639388db9a0717b3715ccc0298ab5c4
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-24 19:10:30 -0700

    created a get post action for redux

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,8 +6,20 @@ export const fetchPosts = () => {
 
     .then(response => response.json())
     .then(posts => {
-
       return dispatch({ type: 'FETCH_POSTS', posts: posts })
-    })
+    });
+  }
+}
+
+
+export const getPost = (postId) => {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_POST'});
+      return fetch('/api/posts/'+ postId)
+
+    .then(response => response.json())
+    .then(post => {
+        return dispatch({type: 'FETCH_POST', post:post})
+    });
   }
 }
