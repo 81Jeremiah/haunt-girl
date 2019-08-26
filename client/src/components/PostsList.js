48e6452db369e211961c78e7281bc23dffa0ac79
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-25 17:55:28 -0700

    added style to PostList

--- client/src/components/PostsList.js
+++ client/src/components/PostsList.js
@@ -5,9 +5,25 @@ const PostsList = (props) =>{
   const listPosts = props.posts.map( post => {
 
     return(
-      <li className='' key={post.id}>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
+      <li className="review-item" key={post.id}>
+        <div class="row">
+					<div class="col-lg-4">
+						<div class="review-pic">
+							<img src="img/review/1.jpg" alt=""/>
+						</div>
+					</div>
+					<div class="col-lg-8">
+						<div class="review-content text-box text-white">
+							<div class="rating">
+								<h5><i>Rating</i><span>4.5</span> / 5</h5>
+							</div>
+							<div class="top-meta">11.11.18  /  in <a href="">Haunts</a></div>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+              <a href="#" class="read-more">Read More  <img src="img/icons/double-arrow.png" alt="#"/></a>
+          </div>
+        </div>
+      </div>
       </li>
     );
   });
