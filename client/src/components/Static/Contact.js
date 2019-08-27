3f195a54d5a5d5c8a59c7ada2bf584dac14c8418
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-26 21:03:31 -0700

    updated links in contact page

--- client/src/components/Static/Contact.js
+++ client/src/components/Static/Contact.js
@@ -1,7 +1,10 @@
 import React,{Component} from 'react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-
+import { Link } from 'react-router-dom';
+import doubleArrow from '../../img/icons/double-arrow.png';
+import locationIcon from '../../img/icons/location.png';
+import emailIcon from '../../img/icons/mail.png';
 
 class Contact extends Component {
 
@@ -13,7 +16,7 @@ class Contact extends Component {
         <div class="page-info">
           <h2>Contact</h2>
           <div class="site-breadcrumb">
-            <a href="">Home</a>  /
+            <Link to="/">Home</Link>  /
             <span>Contact</span>
           </div>
         </div>
@@ -28,22 +31,22 @@ class Contact extends Component {
     						<input type="text" placeholder="Your e-mail"/>
     						<input type="text" placeholder="Subject"/>
     						<textarea placeholder="Message"></textarea>
-    						<button class="site-btn">Send message<img src="img/icons/double-arrow.png" alt="#"/></button>
+    						<button class="site-btn">Send message<img src={doubleArrow}alt="#"/></button>
     					</form>
     				</div>
     				<div class="col-lg-5 order-1 order-lg-2 contact-text text-white">
     					<h3>What up chief, let's talk why not?</h3>
     					<p></p>
     					<div class="cont-info">
-    						<div class="ci-icon"><img src="img/icons/location.png" alt=""/></div>
+    						<div class="ci-icon"><img src={locationIcon} alt=""/></div>
     						<div class="ci-text">PO Box Hollywood CA 90068</div>
     					</div>
     					<div class="cont-info">
-    						<div class="ci-icon"><img src="img/icons/phone.png" alt=""/></div>
+    						<div class="ci-icon"><img src={"img/icons/phone.png"} alt=""/></div>
     						<div class="ci-text"></div>
     					</div>
     					<div class="cont-info">
-    						<div class="ci-icon"><img src="img/icons/mail.png" alt=""/></div>
+    						<div class="ci-icon"><img src={emailIcon} alt=""/></div>
     						<div class="ci-text">thehauntgirl@gmail.com</div>
     					</div>
     				</div>
