ff7ec468ecbbd0bff5432a6871e3eeaa4086f8ae
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-31 15:49:37 -0700

    added new api routes for categories and states

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   namespace :api do
 
    resources :admin, :posts
-
+   resources :categories, only: [:show]
+   resources :states, only: [:index]
 
  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
