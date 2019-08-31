7248d39cef292f491acc8ad8e91ebe850b43ff62
81Jeremiah <jeremiahlutes@gmail.com>
2019-08-31 15:45:02 -0700

    method to find category by params

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ApplicationRecord
   has_many :posts
 
+  def self.find_category_id(param)
+    if param == 'escaperooms'
+      1
+    elsif param == 'haunts'
+      2
+    elsif param == 'oddities'
+      3
+    end
+  end
+
 end
