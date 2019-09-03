f714b7d73d65fcb3b3d2b1f772123df27cf1fb00
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-02 19:20:17 -0700

    added attached_image method to category load

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -7,7 +7,7 @@ class Api::CategoriesController < ApplicationController
     # category = Category.find_by(id: category_id)
 
     @posts = Post.by_category(category_id)
-    render json: @posts
+    render json: @posts.with_attached_image
   end
 
 end
