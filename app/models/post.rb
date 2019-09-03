5d3eaf3dea6ebcb8c63ae64b706a0c6e10cb4e8f
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-02 19:21:29 -0700

    added has one attached to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :state
   scope :by_category, -> (category_id){joins(:category).where("category_id=?", category_id)}
+  has_one_attached :image
 
 end
