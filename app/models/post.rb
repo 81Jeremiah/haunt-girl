57195a1aee1639601dab1b6be7f06a635dabdf67
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-07 20:15:33 -0700

    add scope method to post model to retrieve by state id

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ApplicationRecord
   belongs_to :state
   scope :by_category, -> (category_id){joins(:category).where("category_id=?", category_id)}
   has_one_attached :image
+  scope :by_state, -> (state_id){joins(:state).where("state_id=?", state_id)}
+
 
 end
