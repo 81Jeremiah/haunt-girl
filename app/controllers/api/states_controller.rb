e82280abfe725a845a854695d07071d9540b3037
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-07 20:14:52 -0700

    updated find method to load all posts by state

--- app/controllers/api/states_controller.rb
+++ app/controllers/api/states_controller.rb
@@ -8,7 +8,16 @@ class Api::StatesController < ApplicationController
   def states_with_posts
     states = State.has_posts
     render json: states
+  end
 
+  def show
+    state = State.find_by(abbreviation: params[:id] )
+    state_id = state.id
+
+    # category = Category.find_by(id: category_id)
+
+    posts = Post.by_state(state_id)
+    render json: posts.with_attached_image
   end
 
 end
