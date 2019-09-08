68d50a4292ed024b3331bf4f96bf328f5047a56e
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-07 20:18:24 -0700

    added route in api for states_with_posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :api do
-
+   get '/states/with_posts', to: 'states#states_with_posts'
    resources :admin, :posts
    resources :categories, only: [:show]
-   resources :states, only: [:index]
-   get '/states/with_posts', to: 'states#states_with_posts'
+   resources :states, only: [:index, :show]
+
  end
 
 
