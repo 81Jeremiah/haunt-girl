aefc974e470eb01d3b93254de59b14cb30e3cba2
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-10 21:09:45 -0700

    built out simple search bar component

---
+++ client/src/containers/SearchBar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Button, FormControl, Form } from 'react-bootstrap';
+import { search } from '../actions/searchActions';
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
+
+
+ class SearchBar extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    query: "",
+    redirect: false
+  }
+}
+
+  handleChange = event => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  handleSubmit = event =>{
+    event.preventDefault()
+
+    this.props.search(this.state.query)
+    this.setState({
+      query: "",
+      redirect: true})
+  }
+//redirects to search page once submit button clicked
+  render() {
+    if (this.state.redirect === true){
+      this.setState({
+        query: "",
+        redirect: false})
+      return <Redirect to="/searchresults" />
+    }
+    return(
+      <Form inline onSubmit={this.handleSubmit}>
+        <FormControl type="text" placeholder="Search"
+        className="mr-sm-2" value={this.state.query}  onChange={this.handleChange}/>
+        <Button type="submit"  variant="outline-success">Search</Button>
+      </Form>
+
+
+    )
+
+  }
+}
+
+export default connect(null, {search})(SearchBar);
