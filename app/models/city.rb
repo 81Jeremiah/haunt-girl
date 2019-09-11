c38274a3b856f63ec3f410fa1bd43dcf1d2af9fd
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-10 21:10:39 -0700

    added title case method for city names

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,12 +2,15 @@ class City < ApplicationRecord
   has_many :posts
 
   def self.check_for_city(city)
+    city.titleize
     found_city = City.find_by(name: city)
-    if found_city != nil
-      found_city
-    else
-      new_city = City.create!(name: city)
-      new_city
-    end
 
-  end
+      if found_city != nil
+        found_city
+      else
+        new_city = City.create!(name: city)
+        new_city
+      end
+   end
+
+end
