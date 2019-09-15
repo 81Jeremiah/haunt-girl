af194a08352b699c466795456f2947baf1e7ef2c
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-15 16:12:02 -0700

    added country relationship to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,20 +2,26 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :city
   has_one :state, through: :city
+  has_one :country, through: :state
+
   scope :by_category, -> (category_id){joins(:category).where("category_id=?", category_id)}
   has_one_attached :image
   scope :by_state, -> (state_id){joins(:state).where("state_id=?", state_id)}
   scope :search, ->(query) {where( 'creator_name LIKE ?', "%#{query}%")}
-  accepts_nested_attributes_for :city, :state
 
 
-  def city_name=(name)
-    self.city = City.find_or_create_by(name: name)
+  def city_name=(name) 
+    self.city = City.find_or_create_by(name: name.titleize)
   end
 
   def city_name
     self.city ? self.city.name : nil
   end
+
+  def country_name=(name, country_code)
+    self.state.country = Country.find_or_create_by(name: name, country_code: country_code )
+  end
+
 
   def self.unique_states
     Post.all.collect{ |post| post.state }.uniq
