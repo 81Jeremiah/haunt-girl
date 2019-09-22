a3d8faa597127eb102469592fa9e3cd5162c4f0d
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-22 01:36:41 -0700

    built admin controller

--- app/controllers/api/admins_controller.rb
+++ app/controllers/api/admins_controller.rb
@@ -1,18 +1,27 @@
-class Api::AdminsControllerr < ApplicationController
+class Api::AdminsController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
-
-  def new
-
+  def profile
+    render json: { admin: AdminSerializer.new(current_admin) }, status: :accepted
   end
 
   def create
+    @admin = Admin.create(admin_params)
+    if @admin.valid?
+      @token = encode_token(admid_id: @admin.id)
+      render json: { admin: AdminSerializer.new(@admin), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create admin' }, status: :not_acceptable
+    end
 
   end
 
-  def update
 
+
+
+  private
+  def admin_params
+    params.require(:admin).permit(:username, :password)
   end
-
-
 
 end
