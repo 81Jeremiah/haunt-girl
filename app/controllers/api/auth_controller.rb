9ef629a33715e76eefac97f3a2edd67dec80008c
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-22 01:37:05 -0700

    set up auth controller for JWT token

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,22 @@
+class Api::AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def create
+    @admin = Admin.find_by(username: admin_login_params[:username])
+    #Admin#authenticate comes from BCrypt
+    if @admin && @admin.authenticate(admin_login_params[:password])
+      # encode token comes from ApplicationController
+      token = encode_token({ admin_id: @admin.id })
+      render json: { admin: AdminSerializer.new(@admin), jwt: token }, status: :accepted
+    else
+      render json: { message: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def admin_login_params
+    # params { user: {username: 'Chandler Bing', password: 'hi' } }
+    params.require(:admin).permit(:username, :password)
+  end
+end
