8d955f6bfaf0ba12ce092a9e1555fb3609054c62
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-22 01:40:33 -0700

    added admin and auth routes to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
   namespace :api do
+
    get '/states/with_posts', to: 'states#states_with_posts'
    get  'posts/search' => 'posts#search'
-
-   resources :admins, :posts
+   resources :admins, only: [:create]
+   post '/login', to: 'auth#create'
+   get '/profile', to: 'admin#profile'
+   resources :posts
    get 'posts/:id/edit' => 'posts#edit'
    resources :categories, only: [:show]
    resources :states, only: [:index, :show]
 
  end
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
