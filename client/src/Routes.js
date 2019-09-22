1460db5ea47011d9f3a034163b56ad6df2cc543c
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-22 01:39:17 -0700

    added auth routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -5,7 +5,7 @@ import ProfilePage from './components/Static/About';
 import Posts from './containers/Posts';
 import PostForm from './components/admin/PostForm';
 import Post from './components/Post';
-import EditForm from './components/admin/EditForm';
+import EditContainer from './components/admin/containers/EditContainer';
 import Contact from './components/Static/Contact';
 
 
@@ -16,7 +16,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={ Splash }/>
         <Route exact path="/admin/posts/new" component={ PostForm} />
-        <Route exact path="/admin/posts/:id/edit" component={ EditForm} />
+        <Route exact path="/admin/posts/:id/edit" component={ EditContainer} />
         <Route exact path="/posts/:id" component={Post} />
         <Route exact path="/posts" component={ Posts } />
         <Route exact path="/haunts" component={ Posts } />
