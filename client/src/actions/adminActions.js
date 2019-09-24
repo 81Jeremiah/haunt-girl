91270dee19a0098eb7dd14fa930fbc40f897cd32
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-23 21:07:53 -0700

    created actions for amdin

---
+++ client/src/actions/adminActions.js
@@ -0,0 +1,35 @@
+export const adminLoginFetch = user => {
+  return dispatch => {
+    return fetch("/api/login", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({user})
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.message) {
+          dispatch(handleError(data.message))
+          // Here you should have logic to handle invalid login credentials.
+          // This assumes your Rails API will return a JSON object with a key of
+          // 'message' if there is an error
+        } else {
+          localStorage.setItem("token", data.jwt)
+          dispatch(loginUser(data.user))
+        }
+      })
+  }
+}
+
+const loginUser = userObj => ({
+    type: 'LOGIN_USER',
+    user: userObj
+})
+
+const handleError = msg => ({
+    type: 'LOGIN_ERROR',
+    error: msg
+
+})
