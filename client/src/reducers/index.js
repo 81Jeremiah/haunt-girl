1cc636075068bad58b055435a0440bca7a295a0b
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-24 21:44:22 -0700

    added admin reducer to index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import postReducer from './postReducer';
 import statesReducer from './statesReducer';
 import searchReducer from './searchReducer';
+import adminReducer from './adminReducer'
 
 const rootReducer = combineReducers({
   posts: postReducer,
   states :statesReducer,
-  searchResults: searchReducer
+  searchResults: searchReducer,
+  admin: adminReducer
 
 })
 
