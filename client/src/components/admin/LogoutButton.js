6a81510eb26762ed01a3f91a9c11fddeb399d35b
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-29 13:05:22 -0700

    built logout button component

---
+++ client/src/components/admin/LogoutButton.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {logout} from '../../actions/adminActions'
+
+
+
+class LogoutButton extends Component {
+
+
+  handleClick = (event) => {
+    event.preventDefault()
+    localStorage.removeItem("token")
+    this.props.logout()
+    console.log(this.props)
+
+    this.props.history.replace('/admin/login')
+  }
+
+  render(){
+    return(
+      <button onClick={this.handleClick}>Logout</button>
+    )
+  }
+}
+
+export default connect(null,{logout})(LogoutButton)
