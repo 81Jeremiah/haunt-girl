021e29755a512971bc2cce9cad537f53547e9de4
81Jeremiah <jeremiahlutes@gmail.com>
2019-09-29 13:05:51 -0700

    added an admn route to load a home/profile page

--- client/src/AdminRoutes.js
+++ client/src/AdminRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import PostForm from './components/admin/PostForm';
 import EditContainer from './components/admin/containers/EditContainer';
-
+import Home from './components/admin/Home'
 const Routes = () => {
 
   return (
@@ -11,7 +11,7 @@ const Routes = () => {
 
         <Route exact path="/admin/posts/new" component={ PostForm} />
         <Route exact path="/admin/posts/:id/edit" component={ EditContainer} />
-        
+        <Route exact path="/admin/home" component={ Home } />
       </Switch>
     </Router>
   )
