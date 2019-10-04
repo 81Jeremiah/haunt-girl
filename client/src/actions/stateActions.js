dd054730e70cd8993c1eee9cdcb3709eb53b2afd
81Jeremiah <jeremiahlutes@gmail.com>
2019-10-04 16:42:22 -0400

    updated fetch method for states to add category argument

--- client/src/actions/stateActions.js
+++ client/src/actions/stateActions.js
@@ -1,9 +1,10 @@
-export const fetchStatesWithPosts = () =>{
+export const fetchStatesWithPosts = (category) =>{
   return(dispatch) =>{
     dispatch({type:'LOADING_STATES'});
-    return fetch('api/states/with_posts')
+    return fetch('api/states/with_posts/?id='+ category)
 
-    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+    // resp.json()
     .then(states => {
       dispatch({type:'FETCH_STATES', states: states})
     });
