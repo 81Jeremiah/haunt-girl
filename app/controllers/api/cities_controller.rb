9b3af72523bd4b3ba3038abd3e0ca445e6833b64
81Jeremiah <jeremiahlutes@gmail.com>
2019-10-06 22:07:12 -0400

    fixed param error in cities controller

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,9 +1,9 @@
 class Api::CitiesController < ApplicationController
   skip_before_action :authorized, only: [:cities_in_state]
 
-
   def cities_in_state
-    state = State.find(params[:id])
+    state = State.find_by(abbreviation: params[:id][1..-1] )
+
 
     @cities = state.cities
 
