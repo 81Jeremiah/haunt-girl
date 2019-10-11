a74a3d524e356e7a8da92d41c8e270e4c5fa1d43
81Jeremiah <jeremiahlutes@gmail.com>
2019-10-10 21:16:29 -0700

    fixed links in summary section

--- client/src/components/ShortBio.js
+++ client/src/components/ShortBio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import doubleArrow from '../img/icons/double-arrow.png'
+import {Link} from 'react-router-dom';
 
 const ShortBio = () =>{
   return(
@@ -9,26 +10,26 @@ const ShortBio = () =>{
               <div class="row">
                 <div class="col-md-4">
                   <div class="intro-text-box text-box text-white">
-                    <div class="top-meta"> <a href="">Haunts</a></div>
+                    <div class="top-meta"> <Link to="/haunts">Haunts</Link></div>
                     <h3>2019 Haunt Reviews</h3>
                     <p></p>
-                    <a href="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
+                  <Link to="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></Link>
                   </div>
                 </div>
                 <div class="col-md-4">
                   <div class="intro-text-box text-box text-white">
-                    <div class="top-meta"> <a href="">Escape Rooms</a></div>
+                    <div class="top-meta"> <Link to="/escaperooms">Escape Rooms</Link></div>
                     <h3>Escape Rooms</h3>
                     <p></p>
-                    <a href="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
+                  <Link to="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></Link>
                   </div>
                 </div>
                 <div class="col-md-4">
                   <div class="intro-text-box text-box text-white">
-                    <div class="top-meta"> <a href="">Experiences and more!</a></div>
+                    <div class="top-meta"> <Link to="/oddities">Experiences and more!</Link></div>
                     <h3>Experiences and more!</h3>
                     <p></p>
-                    <a href="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
+                    <Link to="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></Link>
                   </div>
                 </div>
               </div>
