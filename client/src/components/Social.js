c6e64af9324d50a97ab8b82b24066801ec2e5cd9
81Jeremiah <jeremiahlutes@gmail.com>
2019-10-27 16:40:11 -0700

    updated className on class

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Social = () => {
   return(
     <div>
-      <a href="https://www.youtube.com/channel/UCYxmZvq8qVQyq28g5W1EFlQ" target="blank"><i class="fa fa-youtube"></i></a>
-      <a href="https://www.instagram.com/thehauntgirl" target="blank"><i class="fa fa-instagram"></i></a>
-      <a href="https://twitter.com/christinebarger" targer="blank"><i class="fa fa-twitter"></i></a>
+      <a href="https://www.youtube.com/channel/UCYxmZvq8qVQyq28g5W1EFlQ" target="blank"><i className="fa fa-youtube"></i></a>
+    <a href="https://www.instagram.com/thehauntgirl" target="blank"><i className="fa fa-instagram"></i></a>
+  <a href="https://twitter.com/christinebarger" targer="blank"><i className="fa fa-twitter"></i></a>
     </div>
   )
 }
