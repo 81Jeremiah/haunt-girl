cda140e194743d4f9c9519398502f89a26089924
81Jeremiah <jeremiahlutes@gmail.com>
2019-11-25 08:56:52 -0800

    updated featured for hard coded skeleton key

--- client/src/components/FeaturedSection.js
+++ client/src/components/FeaturedSection.js
@@ -7,9 +7,9 @@ const FeaturedSection = () => {
       <div class="featured-bg set-bg" data-setbg="img/featured-bg.jpg"></div>
       <div class="featured-box">
         <div class="text-box">
-          <div class="top-meta">December 27 2019  /  in <a href="">Escape Rooms</a></div>
+          <div class="top-meta"> </div>
           <h3>2019 Skeleton Key Winners</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquamet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum posuere porttitor justo id pellentesque. Proin id lacus feugiat, posuere erat sit amet, commodo ipsum. Donec pellentesque vestibulum metus...</p>
+          <p>Congratulations ! These were the Best!</p>
           <a href="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
         </div>
       </div>
