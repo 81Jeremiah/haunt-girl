9b13af66349dbf88002a472a8a8bfc2e59a3c331
81Jeremiah <jeremiahlutes@gmail.com>
2019-11-25 08:55:43 -0800

    created a nav/header componenet for the admin section

---
+++ client/src/components/admin/AdminHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const AdminHeader = () => {
+  return(
+    <div class="header-bar-warp d-flex">
+      <nav class="top-nav-area w-100">
+        <ul class="main-menu primary-menu">
+          <li className="header-logo">THE HAUNT GIRL</li>
+          <li><Link to="/admin/posts/new">Add New Review </Link></li>
+          <li><Link to="admin/interviews/new">Add New Interviews</Link></li>
+          <li><Link to="admin/TopLists/new">Add New Top Lists</Link></li>
+          <li><Link to="admin/about">About</Link></li>
+          <li><Link to="contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default AdminHeader
