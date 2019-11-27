df8f7a56ebd94a77a45936367333249b7fa4925a
81Jeremiah <jeremiahlutes@gmail.com>
2019-11-27 09:02:18 -0800

    imported header into the admin page

--- client/src/components/admin/Home.js
+++ client/src/components/admin/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LogoutButton from './LogoutButton';
+import AdminHeader from './AdminHeader'
 
 const Home = (props) =>{
   console.log(props.history)
   return(
     <div>
+      <AdminHeader />
       <h1>welcome</h1>
       <LogoutButton history={props.history}/>
     </div>
