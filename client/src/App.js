795de9af7e8d8acf28c921eb0669f5c85b9b23ae
81Jeremiah <jeremiahlutes@gmail.com>
2019-12-05 08:51:15 -0800

    moved a component did update funtion to fix log in bug

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,12 @@ class App extends Component {
     this.props.getProfileFetch()
   }
 
+//   componentDidUpdate(prevProps) {
+//   // Typical usage (don't forget to compare props):
+//   if (this.props.userID !== prevProps.userID) {
+//     this.fetchData(this.props.userID);
+//   }
+// }
 
  render() {
 
