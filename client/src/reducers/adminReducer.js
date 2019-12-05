bd9a6e7ba01d44afd11e605b1f91bfd72b9c8cdb
81Jeremiah <jeremiahlutes@gmail.com>
2019-12-05 08:52:00 -0800

    updated reducers to fix async error of login component not processing authorize

--- client/src/reducers/adminReducer.js
+++ client/src/reducers/adminReducer.js
@@ -2,13 +2,16 @@
   export default ( state =
     { currentUser: {},
       error: "",
-      authorized: false
+      authorized: false,
+      loading: false
     }, action) => {
 
     switch(action.type){
+      case 'LOADING_USER':
+        return {...state, loading: true}
 
       case 'LOGIN_USER':
-        return {...state, currentUser: action.user, authorized: true}
+        return {...state, currentUser: action.user, authorized: true, loading: false}
 
       case 'LOGIN_ERROR':
         return {...state, error: action.error}
