18c81d5727d0e4d3f1e9db73937fcb7745062607
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-06 08:37:31 -0800

    created component to add interviews

---
+++ client/src/components/admin/InterviewForm.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class InterviewForm extends Component {
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default InterviewForm
