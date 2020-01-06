5e1e9de3ef60137b24057cf54c451e77eecdbdd2
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-06 08:37:15 -0800

    created component for TopLists form

---
+++ client/src/components/admin/TopListsForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TopListsForm extends Component {
+  render(){
+    return(
+      <div>
+      Choose state
+      
+      </div>
+    )
+  }
+}
+
+export default TopListsForm
