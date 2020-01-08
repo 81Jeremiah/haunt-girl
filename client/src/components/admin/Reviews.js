461525ad827d5628f1ef36985aea18dea70cde76
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-08 08:40:11 -0800

    changed return to list tags and updated map to call object properly

--- client/src/components/admin/Reviews.js
+++ client/src/components/admin/Reviews.js
@@ -2,19 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 const Reviews = (props) =>{
-
-  const reviewList = this.props.posts.map(post => {
+console.log(props)
+  const reviewList = props.posts.map(post => {
     return(
-      <div key={post.id}>
-        <Link to={`/admin/posts/${post.id}/edit`}>{post.name}</Link>
-      </div>
+      <li key={post.id}>
+        <Link to={`/admin/posts/${post.id}/edit`}>{post.title}</Link>
+      </li>
     )
 
   });
 
   return(
     <div>
-      {reviewList}
+      <ul>
+        {reviewList}
+      </ul>
     </div>
   )
 }
