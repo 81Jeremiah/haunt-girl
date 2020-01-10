23815ca4e98f1c258a0238f2b0f52e060e74fb68
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-10 08:42:39 -0800

    changed haunt logo to link home

--- client/src/components/admin/AdminHeader.js
+++ client/src/components/admin/AdminHeader.js
@@ -6,7 +6,7 @@ const AdminHeader = () => {
     <div class="header-bar-warp d-flex">
       <nav class="top-nav-area w-100">
         <ul class="main-menu primary-menu">
-          <li className="header-logo">THE HAUNT GIRL</li>
+          <li className="header-logo"><Link to="/admin/home">THE HAUNT GIRL</Link></li>
           <li><Link to="/admin/posts/new">Add New Review </Link></li>
           <li><Link to="/admin/interviews/new">Add New Interviews</Link></li>
           <li><Link to="/admin/TopLists/new">Add New Top Lists</Link></li>
