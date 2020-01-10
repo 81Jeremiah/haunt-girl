201cf2c8d9ea0ab4eccd875c0cfd1cb2cff66ec5
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-10 08:43:11 -0800

    added a header to the edit page

--- client/src/components/admin/containers/EditContainer.js
+++ client/src/components/admin/containers/EditContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { getPost } from '../../../actions/postActions';
 import { connect } from 'react-redux';
 import EditForm from '../EditForm';
-
+import AdminHeader from '../AdminHeader'
 class EditContainer extends Component {
 
   componentDidMount(){
@@ -14,11 +14,18 @@ class EditContainer extends Component {
     const title = this.props.post.title
     console.log(this.props.post)
     return(
+     <>
+      <div classname='AdminHeader'>
+        <AdminHeader />
+      </div>
+      <div>
       <EditForm
         post ={this.props.post}
         category={this.props.post.category}
         state={this.props.post.state}
         city={this.props.post.city} />
+      </div>
+    </>
     )
   }
 }
