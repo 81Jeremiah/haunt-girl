5694b621b95a9e058cf33a758b2c48f1078f67f9
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-13 08:32:59 -0800

    update class to className to keep becasue of class keyword conflict

--- client/src/components/BlogSection.js
+++ client/src/components/BlogSection.js
@@ -12,23 +12,23 @@ import TopFive from './TopFive'
 const BlogSection = () => {
 
 return(
-<section class="blog-section spad">
-  <div class="container">
-    <div class="row">
-      <div class="col-xl-9 col-lg-8 col-md-7">
-        <div class="section-title text-white">
+<section className="blog-section spad">
+  <div className="container">
+    <div className="row">
+      <div className="col-xl-9 col-lg-8 col-md-7">
+        <div className="section-title text-white">
           <h2>Top 5 by City</h2>
         </div>
         <TopFive />
         <TopFive />
         <TopFive />
      </div>
-      <div class="col-xl-3 col-lg-4 col-md-5 sidebar">
+      <div className="col-xl-3 col-lg-4 col-md-5 sidebar">
         <div id="stickySidebar">
-          <div class="widget-item">
-            <h4 class="widget-title">The Haunt Girl on Instagram</h4>
-            <div class="trending-widget">
-              <div class="tw-item">
+          <div className="widget-item">
+            <h4 className="widget-title">The Haunt Girl on Instagram</h4>
+            <div className="trending-widget">
+              <div className="tw-item">
                 {/*<Instafeed /> */}
                  <InstaGrid account="thehauntgirl" numberOfMediaElements={10} />
               </div>
