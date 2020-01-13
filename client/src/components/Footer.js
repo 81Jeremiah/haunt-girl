20b5eee9919d7879656223eb243be7dc1e44e9bb
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-13 08:33:23 -0800

    update class to className to keep becasue of class keyword conflict

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,29 +5,29 @@ import HGLogo from '../img/IMG_2792.png'
 const Footer =() => {
 
 return (
-  <footer class="footer-section">
-    <div class="container">
-      <div class="footer-left-pic">
+  <footer className="footer-section">
+    <div className="container">
+      <div className="footer-left-pic">
         <img src="img/footer-left-pic.png" alt=""/>
       </div>
-      <div class="footer-right-pic">
+      <div className="footer-right-pic">
         <img src="img/footer-right-pic.png" alt=""/>
       </div>
-      <a href="#" class="footer-logo">
+      <a href="#" className="footer-logo">
         <img src={HGLogo} alt="" widtch="120" height="120" />
       </a>
-      <ul class="main-menu footer-menu">
+      <ul className="main-menu footer-menu">
         <li><a href="">Home</a></li>
         <li><a href="">Haunt Reviews</a></li>
         <li><a href="">Escape Rooms</a></li>
         <li><a href="">News</a></li>
         <li><a href="">Contact</a></li>
       </ul>
-      <div class="footer-social d-flex justify-content-center">
+      <div className="footer-social d-flex justify-content-center">
          <Social />
 
       </div>
-      <div class="copyright"><a href="">The Haunt Girl</a> 2019 @ All rights reserved</div>
+      <div className="copyright"><a href="">The Haunt Girl</a> 2019 @ All rights reserved</div>
     </div>
   </footer>
  );
