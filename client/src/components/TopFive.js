ab13a7e828d2bb864f6e01ea61f3b90fa9e45b9c
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-13 08:34:12 -0800

    update class to className to keep becasue of class keyword conflict

--- client/src/components/TopFive.js
+++ client/src/components/TopFive.js
@@ -5,10 +5,10 @@ import doubleArrow from '../img/icons/double-arrow.png';
 class TopFive extends Component {
   render(){
     return(
-      <div class="blog-item">
-        <div class="blog-text text-box text-white">
+      <div className="blog-item">
+        <div className="blog-text text-box text-white">
 
-          <div class="top-meta">Top 5 Rooms in <a href="/cities/LosAngeles">Los Angeles</a></div>
+          <div className="top-meta">Top 5 Rooms in <a href="/cities/LosAngeles">Los Angeles</a></div>
           <ul>
           <li>The Basement</li>
           <li>Mad Rapper</li>
@@ -16,7 +16,7 @@ class TopFive extends Component {
           <li>The Artichoke That Time Forgot</li>
           <li>Keep Out of the Potato Salad</li>
           </ul>
-          <a href="/topfie" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
+          <a href="/topfie" className="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
         </div>
       </div>
 
