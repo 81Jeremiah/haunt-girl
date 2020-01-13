604395da9a0badf55f8939559ca3f53db5bc3ed9
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-13 08:33:15 -0800

    update class to className to keep becasue of class keyword conflict

--- client/src/components/FeaturedSection.js
+++ client/src/components/FeaturedSection.js
@@ -3,14 +3,14 @@ import doubleArrow from '../img/icons/double-arrow.png'
 const FeaturedSection = () => {
   return(
 
-    <section class="featured-section">
-      <div class="featured-bg set-bg" data-setbg="img/featured-bg.jpg"></div>
-      <div class="featured-box">
-        <div class="text-box">
-          <div class="top-meta"> </div>
+    <section className="featured-section">
+      <div className="featured-bg set-bg" data-setbg="img/featured-bg.jpg"></div>
+        <div className="featured-box">
+          <div className="text-box">
+            <div className="top-meta"> </div>
           <h3>2019 Skeleton Key Winners</h3>
           <p>Congratulations ! These were the Best!</p>
-          <a href="#" class="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
+          <a href="#" className="read-more">Read More  <img src={ doubleArrow } alt="#"/></a>
         </div>
       </div>
     </section>
