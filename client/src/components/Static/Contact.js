b8404c6b02276f13c22cb5725435a7eef01c4256
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-14 08:38:58 -0800

    updated class attributes to be className

--- client/src/components/Static/Contact.js
+++ client/src/components/Static/Contact.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import doubleArrow from '../../img/icons/double-arrow.png';
 import locationIcon from '../../img/icons/location.png';
 import emailIcon from '../../img/icons/mail.png';
+import phoneIcon from '../../img/icons/phone.png';
 
 class Contact extends Component {
 
@@ -12,42 +13,42 @@ class Contact extends Component {
     return(
     <>
       <Navbar />
-      <section class="page-top-section set-bg" data-setbg="img/page-top-bg/4.jpg">
-        <div class="page-info">
+      <section className="page-top-section set-bg" data-setbg="img/page-top-bg/4.jpg">
+        <div className="page-info">
           <h2>Contact</h2>
-          <div class="site-breadcrumb">
+          <div className="site-breadcrumb">
             <Link to="/">Home</Link>  /
             <span>Contact</span>
           </div>
         </div>
       </section>
-      <section class="contact-page">
-    		<div class="container">
+      <section className="contact-page">
+    		<div className="container">
     			{/*// <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14376.077865872314!2d-73.879277264103!3d40.757667781624285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1546528920522" style="border:0" allowfullscreen></iframe></div> */}
-    			<div class="row">
-    				<div class="col-lg-7 order-2 order-lg-1">
-    					<form class="contact-form">
+    			<div className="row">
+    				<div className="col-lg-7 order-2 order-lg-1">
+    					<form className="contact-form">
     						<input type="text" placeholder="Your name"/>
     						<input type="text" placeholder="Your e-mail"/>
     						<input type="text" placeholder="Subject"/>
     						<textarea placeholder="Message"></textarea>
-    						<button class="site-btn">Send message<img src={doubleArrow}alt="#"/></button>
+              <button className="site-btn">Send message<img src={doubleArrow}alt="#"/></button>
     					</form>
     				</div>
-    				<div class="col-lg-5 order-1 order-lg-2 contact-text text-white">
+    				<div className="col-lg-5 order-1 order-lg-2 contact-text text-white">
     					<h3>What up chief, let's talk why not?</h3>
     					<p></p>
-    					<div class="cont-info">
-    						<div class="ci-icon"><img src={locationIcon} alt=""/></div>
-    						<div class="ci-text">PO Box Hollywood CA 90068</div>
+            <div className="cont-info">
+    						<div className="ci-icon"><img src={locationIcon} alt=""/></div>
+                   <div className="ci-text">PO Box Hollywood CA 90068</div>
     					</div>
-    					<div class="cont-info">
-    						<div class="ci-icon"><img src={"img/icons/phone.png"} alt=""/></div>
-    						<div class="ci-text"></div>
+    					<div className="cont-info">
+    						<div className="ci-icon"><img src={phoneIcon} alt=""/></div>
+                   <div className="ci-text"></div>
     					</div>
-    					<div class="cont-info">
-    						<div class="ci-icon"><img src={emailIcon} alt=""/></div>
-    						<div class="ci-text">thehauntgirl@gmail.com</div>
+    					<div className="cont-info">
+    						<div className="ci-icon"><img src={emailIcon} alt=""/></div>
+              <div className="ci-text">thehauntgirl@gmail.com</div>
     					</div>
     				</div>
     			</div>
