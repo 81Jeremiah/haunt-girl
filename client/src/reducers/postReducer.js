cc26bebe310913bf323dc988bb4b2487e3069e59
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-27 09:06:25 -0800

    created reducers for new update actions

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -6,16 +6,25 @@ export default ( state =
   switch(action.type){
 
     case 'LOADING_POSTS':
-    return {...state, loading: true}
+      return {...state, loading: true}
 
     case 'FETCH_POSTS':
-    return {...state, loading:false, posts: action.posts}
+      return {...state, loading:false, posts: action.posts}
 
     case 'LOADING_POST':
-    return {...state, loading: true}
+      return {...state, loading: true}
 
     case 'FETCH_POST':
-    return {...state, loading:false, post: action.post}
+      return {...state, loading:false, post: action.post}
+
+    case 'SENDING_POST':
+      return {...state, loading: true}
+
+    case 'CREATE_POST':
+      return {...state, loading: false, post: action.post}
+
+    case 'UPDATE_POST':
+        return {...state, loading: false, post: action.post}
 
 
     default:
