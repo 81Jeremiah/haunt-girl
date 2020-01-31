307518e7bfe976b641a8a0cf3edba2434403cfe1
81Jeremiah <jeremiahlutes@gmail.com>
2020-01-31 09:27:22 -0800

    updated and removed auth checks in app component

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,42 @@ import {connect} from 'react-redux';
 import {getProfileFetch} from './actions/adminActions'
 
 
+
+
+// const mapStateToProps = state => {
+// return {
+//   isAuthenticated: state.auth.isAuthenticated,
+//   user: state.auth.currentUser,
+// }
+// }
+//
+// export default connect(mapStateToProps, { checkToken })(App);
+
+
 class App extends Component {
+  // componentWillMount = () => {
+  //   // This conditional will check the users auth_token after they have logged in and make sure they are authenticated
+  //   // looks in localStorage for a token and if true, it authenticates the token
+  //   if (localStorage.getItem("auth_token")) {
+  //     this.props.checkToken(localStorage.getItem("auth_token"))
+  //   // if that fails it will check the window location, if location is anywhere but login, signup, or /
+  //   // it will clear the token and redirect to the login
+  //   } else if (!window.location.href.includes('login') && !window.location.href.includes('signup') && !(window.location.pathname === '/')) {
+  //     localStorage.clear()
+  //     window.location.href = "/login"
+  //   }
+  // }
+  //
+  // render() {
+  //   const {isAuthenticated, user} = this.props
+  //   console.log(isAuthenticated)
+  //
+  //   return (
+  //     <Routes isAuthenticated={isAuthenticated} user={user}/>
+  //   );
+  // }
+  // }
+
   componentWillMount = () => {
     this.props.getProfileFetch()
   }
@@ -26,7 +61,7 @@ class App extends Component {
       <div className="App">
         <>
           <Routes />
-        <AdminRoutes authorized={this.props.user.authorized} />
+          <AdminRoutes authorized={this.props.user.authorized} />
         </>
        </div>
      );
