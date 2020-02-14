be143cd9cecbf17cf0e2d82f4e09dd0d6c886e15
81Jeremiah <jeremiahlutes@gmail.com>
2020-02-14 08:52:10 -0800

    updated classnames to use style from previous pages

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -3,17 +3,43 @@ import ResultList from './ResultList';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import NewsletterSection from '../components/NewsletterSection'
+import { Link } from 'react-router-dom';
+import StatesList from '../components/StatesList';
+
 
 
 import { connect } from 'react-redux'
 
 class SearchResults extends Component {
 
+  handleStateClick = (event) => {
+    event.preventDefault()
+    const chosenState = event.target.innerText
+    this.props.narrowByState(chosenState)
+  }
+
+
    render() {
+
      return (
       <div>
        <Navbar />
-       <ResultList posts={this.props.results} />
+       <section class="page-top-section set-bg" >
+         <div class="page-info">
+
+           <div class="site-breadcrumb">
+             <Link to="/">Home</Link>  /
+             <span>Reviews</span>
+           </div>
+         </div>
+       </section>
+       <section class="review-section">
+         <div class="container">
+          {/*<StatesList states={this.props.states} handleStateClick={this.handleStateClick} />*/}
+          <ResultList posts={this.props.results} />
+       </div>
+     </section>
+
        <NewsletterSection />
        <Footer />
       </div>
