4f82f71351490bb8a94e14035b1b7c3a525a82d3
81Jeremiah <jeremiahlutes@gmail.com>
2020-02-14 08:53:20 -0800

    removed custom css and updatedto reuse established css

--- client/src/components/ResultList.js
+++ client/src/components/ResultList.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ResultsList = props  => {
-
+  console.log(props)
 //list the creators that meet searh criteria
   const listResults = props.posts.map( post => {
     return(
 
-      <li className="search-list" key={post.id}>
-      <Link to={`posts/${post.id}`}>{post.title}</Link>
+      <li className="review-item" key={post.id}>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="review-pic">
+            <img src="img/review/1.jpg" alt=""/>
+          </div>
+        </div>
+        <div class="col-lg-8">
+        <div class="review-content text-box text-white">
+          <div class="rating">
+        </div>
+          <h3><Link to={`posts/${post.id}`}>{post.title}</Link></h3>
+        <div class="review-content text-box text-white"></div>
+      </div>
+    </div>
+  </div>
+
       </li>)
     })
   return(
