40d4370fbc656f47e667db5a56fed2e970915078
81Jeremiah <jeremiahlutes@gmail.com>
2020-02-26 09:10:02 -0800

    added component to list fetched winner information

---
+++ client/src/components/AwardsList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import doubleArrow from '../img/icons/double-arrow.png';
+import { Link } from 'react-router-dom';
+import ReactHtmlParser from 'react-html-parser';
+
+
+
+const AwardsList = (props) =>{
+
+  const listWinners = props.winners.map( winner => {
+
+    return(
+
+      <li className="review-item" key={winner.id}>
+        <div className="row">
+					<div className="col-lg-4">
+						<div className="review-pic">
+							<img src="img/review/1.jpg" alt=""/>
+						</div>
+					</div>
+					<div className="col-lg-8">
+						<div className="review-content text-box text-white">
+							<div className="rating">
+								<h5><i>Rating</i><span>4.5</span> / 5</h5>
+							</div>
+							<div className="top-meta">{post.published_at}  /  in <Link to={props.categoryRoute}>{post.category.name}</Link></div>
+              <h3>{post.title}</h3>
+            <div className="review-content text-box text-white">
+                    {ReactHtmlParser(post.content.substring(0,250))}
+                  </div>
+                   <br/>
+                 <Link to={`${props.categoryRoute}/${post.id}`} className="read-more">Read More  <img src={doubleArrow} alt=""/></Link>
+          </div>
+        </div>
+      </div>
+      </li>
+    );
+  });
+
+  return(
+    <div>
+      <ul>
+        { listWinners }
+      </ul>
+    </div>
+  );
+}
+
+export default AwardsList
