9a5149a3d9d8a96338d4798acc09a5af8ec265ec
81Jeremiah <jeremiahlutes@gmail.com>
2020-02-28 08:52:48 -0800

    added from component and handlers to skeleton key form

--- client/src/components/admin/SkeletonKeyForm.js
+++ client/src/components/admin/SkeletonKeyForm.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
 import Editor from './Editor'
 import AdminHeader from '.AdminHeader'
+import {connect} from 'react-redux';
+import {createAward} from '../../actions/awardsActions';
+import Form from 'react-bootstrap/Form';
 
 class SkeletonKeyForm extends Component{
 
@@ -9,12 +12,37 @@ class SkeletonKeyForm extends Component{
     link: "",
 }
 
+  handleSubmit = event =>{
+    event.preventDefault()
+    this.createAward(this.state)
+  }
+
+  onChange = event =>{
+    const {name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
   render(){
   return(
     <>
     <div><AdminHeader /></div>
+    <Form onSubmit={this.handleSubmit}>
+    <Form.Group controlId="">
+      <Form.Label column sm={2}>Title</Form.Label>
+      <Form.Control
+        size="lg"
+        type="text"
+        value={this.state.title}
+         onChange={this.handleChange}
+         name="title"/>
+    </Form.Group>
     <div><Editor /> </div>
+    </Form>
     </>
   );
  }
 }
+
+export default connect(null,{createAward})(SkeletonKeyForm)
