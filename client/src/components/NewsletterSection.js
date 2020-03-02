c6f6d422c57211f934eb4b094580f3c8116b3a08
81Jeremiah <jeremiahlutes@gmail.com>
2020-03-01 18:02:40 -0800

    text changes on the newletter component

--- client/src/components/NewsletterSection.js
+++ client/src/components/NewsletterSection.js
@@ -5,7 +5,7 @@ const NewsletterSection = () => {
   return(
     <section className="newsletter-section">
       <div className="container">
-        <h2>Subscribe to my newsletter</h2>
+        <h2>Subscribe to the newsletter</h2>
       <form className="newsletter-form">
           <input type="text" placeholder="ENTER YOUR E-MAIL"/>
         <button className="site-btn">subscribe  <img src={doubleArrow} alt="#"/></button>
