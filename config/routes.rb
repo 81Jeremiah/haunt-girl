fba1aef3badee6573347bd97b8ade022929eb823
81Jeremiah <jeremiahlutes@gmail.com>
2020-03-08 18:12:48 -0700

    added api routes to hit interview enpoints

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
    resources :categories, only: [:show]
    resources :states, only: [:index, :show]
    resources :awards, only: [:create, :index, :show]
+   resources :interviews, only: [:create, :index, :show]
  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
