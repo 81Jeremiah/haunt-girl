545e3634bb3eb13e63e48ee52345d5ccb2737e3f
81Jeremiah <jeremiahlutes@gmail.com>
2020-03-08 18:08:50 -0700

    created interview form

--- client/src/components/admin/InterviewForm.js
+++ client/src/components/admin/InterviewForm.js
@@ -1,12 +1,66 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
+import Editor from './Editor'
+import {connect} from 'react-redux';
+import {createAward} from '../../actions/awardActions';
+import Form from 'react-bootstrap/Form';
 
-class InterviewForm extends Component {
-  render(){
-    return(
-      <div>
-      </div>
-    )
-  }
+class InterviewForm extends Component{
+  state = {
+
+      title: "",
+      content: "",
+      website:""
+
 }
 
-export default InterviewForm
+
+  handleSubmit = event =>{
+    event.preventDefault()
+    const interview = this.state
+    this.props.createInterview({'interview': interview})
+  }
+
+  handleChange = event =>{
+    const {name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  getEditorText = (editorText) =>{
+    this.setState({
+      content: editorText
+    })
+  };
+
+  render(){
+  return(
+    <>
+    <Form onSubmit={this.handleSubmit}>
+    <Form.Group controlId="">
+      <Form.Label column sm={2}>Title</Form.Label>
+      <Form.Control
+        size="lg"
+        type="text"
+        value={this.state.title}
+        onChange={this.handleChange}
+        name="title"/>
+    </Form.Group>
+    <Form.Group controlId="">
+      <Form.Label column sm={2}>Website</Form.Label>
+      <Form.Control
+        size="lg"
+        type="text"
+        value={this.state.website}
+        onChange={this.handleChange}
+        name="websitet"/>
+    </Form.Group>
+    <div><Editor content={this.state.content} getEditorText={this.getEditorText}/> </div>
+     <input type="submit" value='Create Interview'/>
+    </Form>
+    </>
+  );
+ }
+}
+
+export default connect(null,{createAward})(InterviewForm)
