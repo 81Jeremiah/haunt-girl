0884da2f910eeef814abbb178be12e33e1d58773
81Jeremiah <jeremiahlutes@gmail.com>
2020-03-15 16:57:53 -0700

    created componet for top lists form

--- client/src/components/admin/TopListsForm.js
+++ client/src/components/admin/TopListsForm.js
@@ -1,14 +1,71 @@
 import React, {Component} from 'react';
-
+import Editor from './Editor'
+import {connect} from 'react-redux';
+import {createAward} from '../../actions/awardActions';
+import Form from 'react-bootstrap/Form';
+import EscapeRoomSearch from './EscapeRoomSearch'
 class TopListsForm extends Component {
-  render(){
-    return(
-      <div>
-      Choose state
-      
-      </div>
-    )
+  state = {
+
+      title: "",
+      area: ""
+
   }
-}
+
+
+    handleSubmit = event =>{
+      event.preventDefault()
+      const award = this.state
+      console.log(award)
+      console.log( {'award': award})
+
+      this.props.createAward({'award': award})
+    }
+
+    handleChange = event =>{
+      const {name, value } = event.target;
+      this.setState({
+        [name]: value
+      })
+    }
+
+    getEditorText = (editorText) =>{
+      this.setState({
+        content: editorText
+      })
+    };
+
+    render(){
+    return(
+      <>
+      <Form onSubmit={this.handleSubmit}>
+      <Form.Group controlId="">
+        <Form.Label column sm={2}>Title</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          value={this.state.title}
+          onChange={this.handleChange}
+          name="title"/>
+      </Form.Group>
+      <Form.Group controlId="">
+        <Form.Label column sm={2}>Year</Form.Label>
+        <Form.Control
+          size="lg"
+          type="text"
+          value={this.state.area}
+          onChange={this.handleChange}
+          name="year"/>
+      </Form.Group>
+      <EscapeRoomSearch />
+      <div><Editor content={this.state.content} getEditorText={this.getEditorText}/> </div>
+       <input type="submit" value='Create List'/>
+      </Form>
+      </>
+    );
+   }
+  }
+
+
 
 export default TopListsForm
