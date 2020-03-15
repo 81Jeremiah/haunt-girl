5fba694d22459182ad4fd3579985f4af2a7cc948
81Jeremiah <jeremiahlutes@gmail.com>
2020-03-14 20:42:08 -0700

    added award and interview reducers to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,13 +4,18 @@ import statesReducer from './statesReducer';
 import searchReducer from './searchReducer';
 import adminReducer from './adminReducer';
 import cityReducer from './cityReducer';
+import awardReducer from './awardReducer';
+import interviewReducer from './interviewReducer';
 
 const rootReducer = combineReducers({
   posts: postReducer,
   states :statesReducer,
   searchResults: searchReducer,
   admin: adminReducer,
-  cities: cityReducer
+  cities: cityReducer,
+  awards: awardReducer,
+  interviews: interviewReducer
+
 })
 
 
