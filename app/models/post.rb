b1a0a104237b2f631ac2e91cb59d4fabf1cf804b
81Jeremiah <jeremiahlutes@gmail.com>
2020-03-31 18:02:17 -0700

    added note to flip cat_id for produciton

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,8 @@ class Post < ApplicationRecord
   has_one_attached :image
   scope :by_state, -> (state_id){joins(:state).where("state_id=?", state_id)}
   scope :search, ->(query) {where( 'lower(title) LIKE ?', "%#{query}%".downcase)}
-  scope :find_escape_rooms, ->(query){joins(:category).where(["category_id=? and lower(title) LIKE ?", 1 ,"%#{query}%".downcase ])}
+  #remember to switch the category to 1
+  scope :find_escape_rooms, ->(query){joins(:category).where(["category_id=? and lower(title) LIKE ?", 2 ,"%#{query}%".downcase ])}
   # scope :find_escape_rooms, ->(query){where("title LIKE ?","%#{query}%" )}
 
 
