dd9f6d7942773b617fed374c9872a0868cf8c6e7
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-02 20:38:08 -0700

    fixed push to spread opererator to add element to an array in the state

--- client/src/components/admin/TopListsForm.js
+++ client/src/components/admin/TopListsForm.js
@@ -3,14 +3,17 @@ import Editor from './Editor'
 import {connect} from 'react-redux';
 import {createList} from '../../actions/listActions';
 import Form from 'react-bootstrap/Form';
-import EscapeRoomSearch from './EscapeRoomSearch'
+import EscapeRoomSearch from './EscapeRoomSearch';
+import AddedEscapeRooms from './AddedEscapeRooms';
+
 class TopListsForm extends Component {
   state = {
 
       title: "",
       area: "",
       escapeRoom:{},
-      escapeRooms: []
+      escapeRooms: [],
+      found:false
   }
 
 
@@ -22,6 +25,14 @@ class TopListsForm extends Component {
       this.props.createList({'list': list})
     }
 
+    foundEscapeRoom = (escapeRoom) =>{
+      this.setState({
+        escapeRooms: [...this.state.escapeRooms, escapeRoom]
+        // found: true
+      })
+      console.log(this.state.escapeRooms)
+    }
+
     handleChange = event =>{
       const {name, value } = event.target;
       this.setState({
@@ -29,56 +40,38 @@ class TopListsForm extends Component {
       })
     }
 
-    getEditorText = (editorText) =>{
-      this.setState({
-        content: editorText
-      })
-    };
 
-    addEscapeRoom = (escapeRoom) =>{
-      this.setState({
-        escapeRoom:escapeRoom
-      })
-    }
-
-    addIngredient = event => {
-      event.preventDefault()
-      const { escapeRooms, escapeRoom } = this.state;
-      escapeRooms.push(escapeRoom)
-
-      this.setState({
-        escapeRooms,
-        escapeRoom: ''
-      }, () => console.log(escapeRooms))
-    }
 
     render(){
+      
     return(
       <>
       <EscapeRoomSearch
-          addEscapeRoom={this.addEscapeRoom} />
+          addEscapeRoom={this.addEscapeRoom}
+          foundEscapeRoom={this.foundEscapeRoom} />
 
       <Form onSubmit={this.handleSubmit}>
-      <Form.Group controlId="">
-        <Form.Label column sm={2}>Title</Form.Label>
-        <Form.Control
-          size="lg"
-          type="text"
-          value={this.state.title}
-          onChange={this.handleChange}
-          name="title"/>
-      </Form.Group>
-      <Form.Group controlId="">
-        <Form.Label column sm={2}>Year</Form.Label>
-        <Form.Control
-          size="lg"
-          type="text"
-          value={this.state.area}
-          onChange={this.handleChange}
-          name="year"/>
-      </Form.Group>
+        <Form.Group controlId="">
+          <Form.Label column sm={2}>Title</Form.Label>
+          <Form.Control
+            size="lg"
+            type="text"
+            value={this.state.title}
+            onChange={this.handleChange}
+            name="title"/>
+        </Form.Group>
+        <Form.Group controlId="">
+          <Form.Label column sm={2}>Year</Form.Label>
+          <Form.Control
+            size="lg"
+            type="text"
+            value={this.state.area}
+            onChange={this.handleChange}
+            name="year"/>
+        </Form.Group>
 
-       <input type="submit" value='Create List'/>
+        <AddedEscapeRooms escapeRooms={this.state.escapeRooms} />
+        <input type="submit" value='Create List'/>
       </Form>
       </>
     );
