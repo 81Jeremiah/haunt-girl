1bd50cfbda42737a8541d27a8850b70cf0cc9021
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-06 20:06:55 -0700

    created list controller

---
+++ app/controllers/api/top_lists_controller.rb
@@ -0,0 +1,28 @@
+class Api::TopListsController < ApplicationController
+  skip_before_action :authorized, only: [:index, :create, :show]
+
+
+  def index
+    lists = TopList.all
+    render json: lists
+  end
+
+  def create
+
+    list = TopList.create(list_params)
+    render json: list
+
+  end
+
+  def show
+    list = TopList.find_by(id: param[:id])
+    render json: list
+  end
+
+  private
+
+  def award_params
+    params.require(:top_list).permit(:title, :area, :escapeRooms)
+  end
+
+end
