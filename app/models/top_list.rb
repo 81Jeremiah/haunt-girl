88d8e055d0f51ad395ae8ad33ff77c92caae8604
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-11 18:22:47 -0700

    added posts attributes method to add mant posts through the list

--- app/models/top_list.rb
+++ app/models/top_list.rb
@@ -1,6 +1,16 @@
 class TopList < ApplicationRecord
   has_many :list_posts
-  has_many :posts, through: :list_post
+  has_many :posts, through: :list_posts
 
+  def posts_attributes=(post_attributes)
+    post_attributes.values.each do |post_attribute|
+    if post_attribute[:title] != ""
+      post = Post.find_or_create_by(post_attribute)
+      self.posts << post
+    else
+      return errors
+    end
+  end
+end
 
 end
