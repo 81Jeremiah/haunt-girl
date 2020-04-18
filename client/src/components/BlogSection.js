7b004311bb7852c743e957a639b5893f3d56227d
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-18 14:02:56 -0700

    moved top 5 splash to blog section

--- client/src/components/BlogSection.js
+++ client/src/components/BlogSection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{Component} from 'react'
 import doubleArrow from '../img/icons/double-arrow.png'
 import add from '../img/add.jpg'
 import blogThumb2 from '../img/blog-widget/2.jpg'
@@ -7,39 +7,52 @@ import blogThumb4 from '../img/blog-widget/4.jpg'
 import InstaGrid from '../components/Instafeed';
 import {Link} from 'react-router-dom'
 import TopFive from './TopFive'
+import TopLists from './TopLists';
+import {fetchLists} from '../actions/listActions';
+import {connect} from 'react-redux';
+
 // import Instafeed from '../components/Instafeed';
 
-const BlogSection = () => {
+class BlogSection extends Component {
 
+  componentDidMount(){
+    this.props.fetchLists()
+
+  }
+render(){
 return(
-<section className="blog-section spad">
-  <div className="container">
-    <div className="row">
-      <div className="col-xl-9 col-lg-8 col-md-7">
-        <div className="section-title text-white">
-          <h2>Top 5 by City</h2>
-        </div>
-        <TopFive />
-        <TopFive />
-        <TopFive />
-     </div>
-      <div className="col-xl-3 col-lg-4 col-md-5 sidebar">
-        <div id="stickySidebar">
-          <div className="widget-item">
-            <h4 className="widget-title">The Haunt Girl on Instagram</h4>
-            <div className="trending-widget">
-              <div className="tw-item">
-                {/*<Instafeed /> */}
-                 <InstaGrid account="thehauntgirl" numberOfMediaElements={10} />
+  <section className="blog-section spad">
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-9 col-lg-8 col-md-7">
+          <div className="section-title text-white">
+            <h2>Top 5 by City</h2>
+          </div>
+            <TopLists lists={this.props.toplists} />
+       </div>
+        <div className="col-xl-3 col-lg-4 col-md-5 sidebar">
+          <div id="stickySidebar">
+            <div className="widget-item">
+              <h4 className="widget-title">The Haunt Girl on Instagram</h4>
+              <div className="trending-widget">
+                <div className="tw-item">
+                  {/*<Instafeed /> */}
+                   <InstaGrid account="thehauntgirl" numberOfMediaElements={10} />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
- )
+  </section>
+  );
+ }
 }
 
-export default BlogSection
+const mapStateToProps = state => {
+  console.log(state)
+  return {toplists: state.lists.lists}
+}
+
+export default connect(mapStateToProps ,{fetchLists})(BlogSection)
