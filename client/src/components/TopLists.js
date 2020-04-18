ec350197247ed3a0cf160dbf05864697ae4fb1f3
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-18 14:04:40 -0700

    fixed top lists to utilize html headers and orderedlists

--- client/src/components/TopLists.js
+++ client/src/components/TopLists.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ListLists from './ListLists';
 // import AreaList from '.AreaList';
 
 const TopLists = (props) =>{
 
   const list = props.lists.map( list => {
-    return <li key={list.id}> {list.title} </li>
+    return <li key={list.id}>
+           <h5>{list.title}</h5>
+           <ol>
+              <ListLists posts={list.posts} />
+           </ol>
+           </li>
   })
 
   return (
