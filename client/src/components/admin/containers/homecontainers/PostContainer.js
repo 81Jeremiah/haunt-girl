4d5d88a346a6a6e50c0a4aa092cee1eb432a0f23
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-22 20:46:33 -0700

    moved home page interview list to it's own component

---
+++ client/src/components/admin/containers/homecontainers/PostContainer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import Reviews from '../../Reviews';
+import {fetchAllPosts} from '../../../../actions/postActions';
+
+class ListReviews extends Component {
+
+  componentDidMount(){
+    this.props.fetchAllPosts()
+    }
+
+  render(){
+    return(
+      <div>
+        <Reviews posts={this.props.posts}/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) =>{
+  console.log(state)
+  return {posts: state.posts.posts}
+}
+export default connect(mapStateToProps, {fetchAllPosts})(ListReviews)
