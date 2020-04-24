48fab0fcbd3a1f728b5cde26e5af238a180c1ab0
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-23 21:13:30 -0700

    added new routes for update admin components

--- client/src/AdminRoutes.js
+++ client/src/AdminRoutes.js
@@ -6,10 +6,12 @@ import Home from './components/admin/Home';
 import InterviewEditContainer from './components/admin/containers/InterviewContainer';
 import TopListsForm from './components/admin/TopListsForm';
 import MyEditor from './components/admin/newEditForm/MyEditor'
-import SkeletonKeyContainer from './components/admin/containers/SkeletonKeyContainer';
+import SkeletonKeyEditContainer from './components/admin/containers/SkeletonKeyContainer';
 import AnnouncementContainer from './components/admin/containers/AnnouncementContainer';
 import PostContainer from './components/admin/containers/homecontainers/PostContainer';
 import InterviewContainer from './components/admin/containers/homecontainers/InterviewContainer';
+import TopListContainer from './components/admin/containers/homecontainers/TopListContainer';
+import SkeletonKeyContainer from './components/admin/containers/homecontainers/SkeletonKeyContainer';
 
 
 const Routes = () => {
@@ -24,10 +26,12 @@ const Routes = () => {
         <Route exact path="/admin/TopLists/new" component={ TopListsForm } />
         <Route exact path='/admin/newEditForm' component= { MyEditor} />
         <Route exact path="/admin/home" component={ Home } />
-        <Route exact path="/admin/awards/new" component={ SkeletonKeyContainer } />
+        <Route exact path="/admin/awards/new" component={ SkeletonKeyEditContainer } />
         <Route exact path="/admin/announcements/new" component={ AnnouncementContainer } />
         <Route exact path="/admin/posts" component={PostContainer } />
-        <Route exact path="/admin/interviews/new" component={ InterviewContainer } />
+        <Route exact path="/admin/interviews/" component={ InterviewContainer } />
+        <Route exact path="/admin/TopLists/" component={ TopListContainer} />
+      <Route exact path="/admin/awards/" component={ SkeletonKeyContainer} />
       </Switch>
     </Router>
   )
