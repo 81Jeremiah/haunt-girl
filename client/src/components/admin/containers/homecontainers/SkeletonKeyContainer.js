6d478d3911da9b36a23f64b7b561cc902efcf619
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-23 21:15:14 -0700

    created skeleton key container

---
+++ client/src/components/admin/containers/homecontainers/SkeletonKeyContainer.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import SkeletonKeyList from '../../SkeletonKeyList';
+import {fetchWinners} from '../../../../actions/awardActions';
+import { Link } from 'react-router-dom';
+import AdminHeader from '../../AdminHeader';
+
+class SkeletonKeyContainer extends Component {
+
+  componentDidMount(){
+    this.props.fetchWinners()
+    }
+
+  render(){
+    return(
+      <>
+      <div className='AdminHeader'>
+        <AdminHeader />
+
+      </div>
+      <div>
+        <SkeletonKeyList awards={this.props.awards}/>
+        <Link to={`/awards/new/`}> Create New </Link>
+
+      </div>
+      </>
+    )
+  }
+}
+
+const mapStateToProps = (state) =>{
+  return {awards: state.awards.awards}
+}
+export default connect(mapStateToProps, {fetchWinners})(SkeletonKeyContainer)
