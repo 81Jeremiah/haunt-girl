bbea67d64ad5c6f33b932e269c3d4e70cc33167a
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-25 18:34:04 -0700

    created container component for announcements

---
+++ client/src/components/admin/containers/homecontainers/AnnouncementContainer.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {fetchAnnouncements} from '../../../../actions/announcementActions';
+import { Link } from 'react-router-dom';
+import AdminHeader from '../../AdminHeader';
+import AnnouncementsList from '../../AnnouncementsList';
+
+class AnnouncementContainer extends Component {
+
+  componentDidMount(){
+    this.props.fetchAnnouncements()
+    }
+
+  render(){
+    return(
+      <>
+      <div className='AdminHeader'>
+        <AdminHeader />
+
+      </div>
+      <div>
+        <AnnouncementsList announcements={this.props.announcements}/>
+        <Link to={`new`}> Create New </Link>
+
+      </div>
+      </>
+    )
+  }
+}
+
+const mapStateToProps = (state) =>{
+  console.log(state)
+  return {announcements: state.announcements.announcements}
+}
+export default connect(mapStateToProps, {fetchAnnouncements})(AnnouncementContainer)
