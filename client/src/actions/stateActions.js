f80a54094b136c25bf5981b4f16e5e4525c1ae2e
81Jeremiah <jeremiahlutes@gmail.com>
2020-04-28 21:59:39 -0700

    fixed fetch link in actions

--- client/src/actions/stateActions.js
+++ client/src/actions/stateActions.js
@@ -1,7 +1,7 @@
 export const fetchStatesWithPosts = (category) =>{
   return(dispatch) =>{
     dispatch({type:'LOADING_STATES'});
-    return fetch('api/states/with_posts/?id='+ category)
+    return fetch('/api/states/with_posts/?id='+ category)
 
     .then(resp => resp.json())
     // resp.json()
