b0f96d054885d64f4f9d5de74534cc7d1dcf86d2
81Jeremiah <jeremiahlutes@gmail.com>
2020-05-21 20:03:32 -0700

    created component top awards of the year

---
+++ client/src/components/TopThreeAwards.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TopThreeAwards extends Component {
+
+   render(){
+     return(
+       <div></div>
+     )
+   }
+
+
+}
+
+export default TopThreeAwards
