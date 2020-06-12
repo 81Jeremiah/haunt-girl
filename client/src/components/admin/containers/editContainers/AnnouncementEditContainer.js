3744d53d10c1afb409626002425eb2ab93e12b77
81Jeremiah <jeremiahlutes@gmail.com>
2020-06-11 19:58:27 -0700

    created a new edit container for announements

---
+++ client/src/components/admin/containers/editContainers/AnnouncementEditContainer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Redirect } from 'react-router-dom'
+import  {getAnnouncement} from '../../../actions/announcementActions'
+import AnnouncementForm from '../AnnouncementForm';
+import AdminHeader from '../AdminHeader'
+
+class AnnouncementEditContainer extends Component {
+
+  state: {
+    editing: false
+  }
+
+ componentDidMount(){
+   const announcementId = this.props.match.params.id
+   this.props.getAnnouncement(announcementId)
+
+ }
+
+
+  render(){
+
+    return(
+     <>
+      <div className='AdminHeader'>
+        <AdminHeader />
+      </div>
+      <div>
+
+      <AnnouncementForm announcement={this.state.announcement}
+
+
+        />
+
+      </div>
+    </>
+    )
+  }
+}
+
+
+  const mapStateToProps = state => {
+      return {announcement: state.announcement.announcement}
+  };
+
+
+
+
+export default AnnouncementEditContainer = withRouter(connect(mapStateToProps, {getAnnouncement} )(AnnouncementEditContainer));
