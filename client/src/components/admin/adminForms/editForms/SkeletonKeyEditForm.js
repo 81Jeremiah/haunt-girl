bfb49c8a003f87616d9138d73f7e7dcb7bad4302
81Jeremiah <jeremiahlutes@gmail.com>
2020-06-22 19:30:49 -0700

    created a skeleton key edit from component

---
+++ client/src/components/admin/adminForms/editForms/SkeletonKeyEditForm.js
@@ -0,0 +1,94 @@
+import React,{Component} from 'react';
+import Editor from './Editor'
+import {connect} from 'react-redux';
+import {updateAward} from '../../actions/awardActions';
+import Form from 'react-bootstrap/Form';
+
+class SkeletonKeyEditForm extends Component{
+  state = {
+
+      title: "",
+      link_to_post: "",
+      content: "",
+      year:"",
+      id: ""
+
+  }
+
+
+  handleSubmit = event =>{
+    event.preventDefault()
+    const award = this.state
+    const awardId = this.state.id
+    console.log(award)
+    console.log( {'award': award})
+
+    this.props.updateAward(awardId, {'award': award})
+  }
+
+  handleChange = event =>{
+    const {name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  getEditorText = (editorText) =>{
+    this.setState({
+      content: editorText
+    })
+  };
+
+  componentDidUpdate = (prevProps) => {
+    if(prevProps !== this.props){
+      this.setState({
+        title: this.props.award.title || "",
+        link_to_post: this.props.award.link_to_post || "",
+        content: this.props.award.content || "",
+        year:this.props.award.year || "",
+        id:this.props.award.id || ""
+
+      });
+    }
+  }
+
+  render(){
+  return(
+    <>
+    <Form onSubmit={this.handleSubmit}>
+    <Form.Group controlId="">
+      <Form.Label column sm={2}>Title</Form.Label>
+      <Form.Control
+        size="lg"
+        type="text"
+        value={this.state.title}
+        onChange={this.handleChange}
+        name="title"/>
+    </Form.Group>
+    <Form.Group controlId="">
+      <Form.Label column sm={2}>Year</Form.Label>
+      <Form.Control
+        size="lg"
+        type="text"
+        value={this.state.year}
+        onChange={this.handleChange}
+        name="year"/>
+    </Form.Group>
+    <Form.Group controlId="">
+      <Form.Label column sm={2}>Link</Form.Label>
+      <Form.Control
+        size="lg"
+        type="text"
+        value={this.state.link_to_post}
+        onChange={this.handleChange}
+        name="link_to_post"/>
+    </Form.Group>
+    <div><Editor content={this.state.content} getEditorText={this.getEditorText}/> </div>
+     <input type="submit" value='Create Award'/>
+    </Form>
+    </>
+  );
+ }
+}
+
+export default connect(null,{updateAward})(SkeletonKeyEditForm)
