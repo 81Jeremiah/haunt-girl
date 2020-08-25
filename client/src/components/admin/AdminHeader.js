a43469c44287035c5ed79e403040e993a2c1f62d
81Jeremiah <jeremiahlutes@gmail.com>
2020-08-24 20:33:47 -0700

    added drop down in admin to account split reviews into categories

--- client/src/components/admin/AdminHeader.js
+++ client/src/components/admin/AdminHeader.js
@@ -9,7 +9,14 @@ const AdminHeader = (props) => {
       <nav className="top-nav-area w-100">
         <ul className="main-menu primary-menu">
           <li className="header-logo"><Link to="/admin/home">THE HAUNT GIRL</Link></li>
-          <li><Link to="/admin/posts/">Reviews </Link></li>
+          <li><Link to="/admin/posts/">Reviews </Link>
+            <ul className="sub-menu">
+               <li><Link to="/admin/reviews/haunts">Haunts </Link></li>
+               <li><Link to="/admin/reviews/escaperooms">Escape Rooms</Link></li>
+               <li><Link to="admin/reviews/oddities">Oddities</Link></li>
+               <li><Link to="admin/reviews/immersives">Immersives</Link></li>
+            </ul>
+          </li>
           <li><Link to="/admin/interviews/">Interviews</Link></li>
           <li><Link to="/admin/TopLists/">Top Lists</Link></li>
           <li><Link to="/admin/awards/">Skeleton Keys</Link></li>
