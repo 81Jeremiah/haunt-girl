df2cdb8ef70ba2f4d8852f642219168d5cf8a541
81Jeremiah <jeremiahlutes@gmail.com>
2020-08-31 20:55:30 -0700

    added a listerner to delete an interview

--- client/src/components/admin/InterviewList.js
+++ client/src/components/admin/InterviewList.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import {deleteInterview} from '../../actions/interviewActions';
 
 
 const InterviewsList = (props) =>{
+
+  const handleDelete = (interviewId) => {
+    props.deleteInterview(interviewId)
+  }
 
   const listInterviews = props.interviews.map( interview => {
     return(
       <li key={interview.id}>
         <Link to={`${interview.id}/edit`}>{interview.title}</Link>
+       <span onClick={() => handleDelete(interview.id)}>(x)</span>
       </li>
     );
   });
@@ -22,4 +29,4 @@ const InterviewsList = (props) =>{
   );
 }
 
-export default InterviewsList
+export default connect(null,{deleteInterview})(InterviewsList)
