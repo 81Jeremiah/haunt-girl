558fc36ecc2228615242e6241ee4fe833ac34a3b
81Jeremiah <jeremiahlutes@gmail.com>
2020-09-02 20:52:07 -0700

    added delete button to list reviews

--- client/src/components/admin/Reviews.js
+++ client/src/components/admin/Reviews.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import {deletePost} from '../../actions/postActions';
 
 const Reviews = (props) =>{
-console.log(props)
+
+
+  const handleClick = (escaperoom) => {
+    props.deleteEscapeRoom(escaperoom)
+  }
+
+
   const reviewList = props.posts.map(post => {
     return(
       <li key={post.id}>
         <Link to={`/admin/posts/${post.id}/edit`}>{post.title}</Link>
+        <span onClick={() => handleClick(post.id)}>(x)</span>
       </li>
     )
 
@@ -21,4 +30,4 @@ console.log(props)
   )
 }
 
-export default Reviews
+export default connect(null,{deletePost})(Reviews)
