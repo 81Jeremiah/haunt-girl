d4235b15d5eb5c01fe34bbaa9ac71572c79a1d33
81Jeremiah <jeremiahlutes@gmail.com>
2020-10-09 18:47:05 -0700

    created lists component to show TopLists on list page

---
+++ client/src/components/TopList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'react-router';
+
+const TopList = (props) =>{
+  const lists = props.lists
+  const listOfLists = lists.map(list => {
+    return(
+      <li key={list.id}>
+      <h5><Link to={`/blog/topslist/${list.id}`}>{list.title}></Link></h5>
+      </li>
+    )
+
+
+  })
+
+
+  return(
+    <ul>
+     {listOfLists}
+    </ul>
+  )
+}
+
+
+export default TopList
