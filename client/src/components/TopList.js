da6618ce29a0f75e06a9eeb0b0ed80dbdbb3f61a
81Jeremiah <jeremiahlutes@gmail.com>
2020-10-11 15:36:04 -0700

    corrected module being imported

--- client/src/components/TopList.js
+++ client/src/components/TopList.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Link from 'react-router';
+import { Link } from 'react-router-dom';
 
 const TopList = (props) =>{
   const lists = props.lists
   const listOfLists = lists.map(list => {
     return(
       <li key={list.id}>
-      <h5><Link to={`/blog/topslist/${list.id}`}>{list.title}></Link></h5>
+      <h5><Link to={`/blog/topslist/${list.id}`}>{list.title}</Link></h5>
       </li>
     )
 
