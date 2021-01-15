cf53d457cdb13c75bf0e105da5f44c67c5a196e2
81Jeremiah <jeremiahlutes@gmail.com>
2021-01-14 21:01:24 -0800

    added access token

--- client/src/components/BlogSection.js
+++ client/src/components/BlogSection.js
@@ -37,7 +37,10 @@ return(
               <div className="trending-widget">
                 <div className="tw-item">
                   {/*<Instafeed /> */}
-                   <InstaGrid account="thehauntgirl" numberOfMediaElements={10} />
+                   <InstaGrid
+                     account="thehauntgirl"
+                     numberOfMediaElements={10}
+                     clientAccessToken = "443969603407336|ef9a75dd3990ae419419212aecf84da9"/>
                 </div>
               </div>
             </div>
